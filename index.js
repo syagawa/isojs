23d7728d2b56ed2e33050ce3bf6cc96a4cbf281f
syagawa <yagawa@qzc.co.jp>
2018-03-12 21:22:15 +0900

    p28のロジック、実際には動かず

--- index.js
+++ index.js
@@ -3,3 +3,23 @@
 var formatDate = require('./formatDate.js');
 console.log(formatDate(Date.now()));
 
+var http = require('http');
+
+
+// var redirect = require('shared-redirect');
+
+// if(isRedirectRequired){
+//   redirect('http://www.oreilly.comt');
+// }
+
+// if(window){
+//   window.location.href = 'http://www.oreilly.com';
+// }else{
+//   http.createServer(function(req, res){
+//     console.log(req.path);
+//     res.writeHead(302, {'Location': 'http://www.oreilly.com'});
+//     res.end();
+//   }).listen(1337, '127.0.0.1');
+
+  this._res.writeHead(302, {'Location': 'http://www.oreilly.com'});
+// }
